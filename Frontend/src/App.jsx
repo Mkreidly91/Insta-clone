3f833358ed3edfe8080e6993f1c953031e2b18dd
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-09 01:48:42 +0300

    Frontend <> Added boiler plate vite app, deleted some useless files, and added necessary packages

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="text-5xl">hello</p>
+    </>
+  );
+}
+
+export default App;
